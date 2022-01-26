10ebee2b269b3d105d38759ef688beb435dc4c22
yen001004 <yenn001004@gmail.com>
2022-01-26 13:51:11 +0900

    Register page

---
+++ client/src/App.js
@@ -0,0 +1,46 @@
+import React from 'react';
+import './App.css';
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+  //Link
+} from "react-router-dom";
+
+import LandingPage from './components/views/LandingPage/LandingPage';
+import LoginPage from './components/views/LoginPage/LoginPage';
+import RegisterPage from './components/views/RegisterPage/RegisterPage';
+
+function App() {
+  return (
+    <Router>
+      <div>
+        
+
+        {/*
+          A <Switch> looks through all its children <Route>
+          elements and renders the first one whose path
+          matches the current URL. Use a <Switch> any time
+          you have multiple routes, but you want only one
+          of them to render at a time
+        */}
+        <Routes>
+          <Route exact path="/" element = {<LandingPage/>} />
+          <Route path="/login" element = {<LoginPage/>} />
+          <Route path="/register" element = {<RegisterPage/>} />
+        </Routes>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
+
+function Home() {
+  return (
+    <div>
+      <h2>Home</h2>
+    </div>
+  );
+}
+
